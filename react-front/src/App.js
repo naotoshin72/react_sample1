37445330e97b75da4aa35c0055e7712f56128b20
Naotoshi Nishijima <naotoshi-nishijima@cubic-hd.co.jp>
2017-09-17 13:02:09 +0900

    react-router-domを追加

--- react-front/src/App.js
+++ react-front/src/App.js
@@ -1,34 +1,20 @@
 import React, { Component } from 'react';
-import ReactDOM, {render} from 'react-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
-import { Router, Route, IndexRoute, IndexLink, Link } from 'react-router';
-
-import logo from './logo.svg';
-import './App.css';
-import Sample1 from './components/Sample1.js';
-import Sample2 from './components/Sample2.js';
-
+import Home from './Home'
 import PageA from './PageA'
 import PageB from './PageB'
-
-var destination = document.querySelector("#container");
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <Router>
+        <div>
+          <Route exact path="/" component={Home} />
+          <Route path="/PageA" component={PageA} />
+          <Route path="/PageB" component={PageB} />
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Sample1 />
-        <Sample2 />
-
-      </div>
-
+      </Router>
     );
   }
 }
